69ae71f72efc293f35439e5eefc283938a1d389a
TrueTechy <paulgourlay16@gmail.com>
2017-01-16 18:10:05 +0000

    Created Mines and added flag feature

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import random
 # Dummy function for feature implementation tracking
 def doNothing(feature):
     print("Feature not Implemented: " + feature)
@@ -8,37 +8,80 @@ def doNothing(feature):
 class Field:
 
     board=[]
+    height = 15
+    width = 20
+    noOfMines = 30
 
     def __init__(self, base):
         fieldFrame = Frame(base)
         fieldFrame.pack(side=BOTTOM)
-        for row in range(0,15):
+        for row in range(0,self.height):
             self.board.append([])
-            for column in range(0,20):
-                self.board[row].append(Box(self.board, fieldFrame, row, column))
+            for column in range(0,self.width):
+               self.board[row].append(Box(self.board, fieldFrame, row, column))
+
+        self.plantMines()
+
+    def plantMines(self):
+        while(self.noOfMines>0):
+            for row in range(0, self.height):
+                for column in range(0, self.width):
+                        if (random.randint(0, 100) < 2):
+                            if(self.board[row][column].containsMine==False):
+                                self.board[row][column].containsMine = True
+                                self.noOfMines = self.noOfMines - 1
+                                if(self.noOfMines==0):
+                                    return
 
 
+    def showMines(self):
+        for row in range(0, self.height):
+            for column in range(0, self.width):
+                if(self.board[row][column].containsMine==True):
+                    self.board[row][column].button.config(relief=SUNKEN)
 
 
 class Box(Field):
 
     opened = False
-    containsMine = True
+    containsMine = False
     flagged = False
+    nearbyMines = 0
 
     def __init__(self, field, frame, row, column):
         self.gameField = field
         self.button = Button(frame, width=2, height=1)
+        self.button.config(bg="white smoke")
         self.row = row
         self.column = column
         self.button.grid(row=row, column=column)
-        self.button.bind("<Button-1>", self.openBox())
+        self.button.bind("<Button-1>", self.openBox)
+        #self.button.bind("<Button-2>", self.helper)
+        self.button.bind("<Button-3>", self.flagBox)
+        ## TODO: Calculate nearby mines somehow
 
-## Running function on startup
-    def openBox(self):
+    def openBox(self, event):
+        if(self.flagged==True):
+            return
         self.opened = True
         if(self.containsMine==True):
             print("Game Over")
+        elif(self.nearbyMines==0):
+            ## open box and scan for nearby boxes to open
+            return
+        else:
+            ## Reveal Number
+            print(self.nearbyMines)
+            return
+
+    def flagBox(self, event):
+        if(self.opened==False and self.flagged==False):
+            self.flagged = True
+            self.button.config(bg = "red")
+            return
+        elif(self.opened==FALSE and self.flagged==True):
+            self.flagged = False
+            self.button.config(bg="white smoke")
 
 
 # Create base window
@@ -53,6 +96,6 @@ gameMenu.add_command(label="Exit", command=base.quit)
 
 
 gameField = Field(base)
-
+gameField.showMines()
 
 base.mainloop()
